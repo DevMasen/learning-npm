5ee47f31f410257abb5518a854c3d830365c6b37
DevMasen <mohammadwh400@gmail.com>
2024-09-08 17:21:24 +0330

    docs: Peyvast deleted and gitignore updated

--- Peyvast/04-module/js/index.js
+++
@@ -1,5 +0,0 @@
-//! My Notes
-import { splitParagraph } from './utils.js';
-const paragraph = document.querySelector('p');
-console.log(paragraph.textContent);
-console.log(splitParagraph(paragraph.textContent));
